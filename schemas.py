aa9c4c25ce8b541673ca22ec5e7146001d87d3eb
Elango sakthivel <elangosakthivel0@gmail.com>
2026-04-09 21:53:41 +0530

    Create schemas.py

---
+++ schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+    age: int
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: str
+    age: int
+
+    class Config:
+        from_attributes = True
