f0f87a4119d3613dd6cced2a038932e5d0ab6e1a
Elango sakthivel <elangosakthivel0@gmail.com>
2026-04-11 23:08:32 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -5,21 +5,6 @@ class UserCreate(BaseModel):
     email: str
     age: int
 
-class UserResponse(BaseModel):
-    id: int
-    name: str
-    email: str
-    age: int
-
-    class Config:
-        from_attributes = True
-from pydantic import BaseModel
-
-class UserCreate(BaseModel):
-    name: str
-    email: str
-    age: int
-
 class UserUpdate(BaseModel):
     name: str
     email: str
