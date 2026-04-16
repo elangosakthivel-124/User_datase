d324f455e521211c6c9768f45688a6e9e403aab7
Elango sakthivel <elangosakthivel0@gmail.com>
2026-04-16 23:54:05 +0530

    Update db.py

--- db.py
+++ db.py
@@ -17,3 +17,24 @@ def get_db():
         yield db
     finally:
         db.close()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
