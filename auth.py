c0352ea243a811ea477363016ce374b45ed288d4
Elango sakthivel <elangosakthivel0@gmail.com>
2026-04-17 23:36:36 +0530

    Implement JWT token creation and decoding
    Added functions to create and decode JWT access and refresh tokens using environment variables for configuration.

--- auth.py
+++ auth.py
@@ -22,3 +22,36 @@ def decode_token(token: str):
         return payload
     except JWTError:
         return None
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_EXPIRE = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+REFRESH_EXPIRE = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS"))
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_EXPIRE)
+    to_encode.update({"exp": expire, "type": "access"})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=REFRESH_EXPIRE)
+    to_encode.update({"exp": expire, "type": "refresh"})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def decode_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
