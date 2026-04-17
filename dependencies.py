c357a1e235318377ba3b8805266cd8075e7432be
Elango sakthivel <elangosakthivel0@gmail.com>
2026-04-17 23:38:34 +0530

    Update dependencies.py

--- dependencies.py
+++ dependencies.py
@@ -20,3 +20,25 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         raise HTTPException(status_code=404, detail="User not found")
 
     return user
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from db import get_db
+from auth import decode_token
+import models
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    payload = decode_token(token)
+
+    if payload is None or payload.get("type") != "access":
+        raise HTTPException(status_code=401, detail="Invalid access token")
+
+    email = payload.get("sub")
+    user = db.query(models.User).filter(models.User.email == email).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
