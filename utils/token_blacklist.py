57bf3c0a48ab08ed78676a42ca63a2bb3a5cfe68
Elango sakthivel <elangosakthivel0@gmail.com>
2026-04-18 23:28:43 +0530

    Add simple in-memory token blacklist functionality

---
+++ utils/token_blacklist.py
@@ -0,0 +1,8 @@
+# Simple in-memory blacklist (for learning)
+blacklisted_tokens = set()
+
+def add_to_blacklist(token: str):
+    blacklisted_tokens.add(token)
+
+def is_blacklisted(token: str):
+    return token in blacklisted_tokens
