8ebaa3215fa1681c7b31ada957aa7ecdf060e3e2
Elango sakthivel <elangosakthivel0@gmail.com>
2026-04-19 23:41:11 +0530

    Implement get_current_user function for authentication
    Added a function to retrieve the current user from the token and database, including checks for token validity and user existence.

--- dependencies.py
+++ dependencies.py
@@ -77,3 +77,33 @@ def require_role(required_role: str):
             raise HTTPException(status_code=403, detail="Access denied")
         return user
     return role_checker
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+from db import get_db
+from auth import decode_token
+from utils.redis_client import is_token_blacklisted
+import models
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    if is_token_blacklisted(token):
+        raise HTTPException(status_code=401, detail="Token revoked")
+
+    payload = decode_token(token)
+
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    email = payload.get("sub")
+    user = db.query(models.User).filter(models.User.email == email).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return user
