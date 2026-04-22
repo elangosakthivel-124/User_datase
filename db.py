66bdbdf5c00b3fadb69b1a7177c7c612431d52b7
Elango sakthivel <elangosakthivel0@gmail.com>
2026-04-22 22:20:22 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,24 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from core.config import settings
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    pool_pre_ping=True
-)
-
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False
-)
-
-Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
