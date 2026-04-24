d32037064bd8651a1e961e84496abe55fcd32036
Elango sakthivel <elangosakthivel0@gmail.com>
2026-04-24 21:36:41 +0530

    Update and rename users to users.py

---
+++ users.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from app.db.base import Base
+from fastapi import APIRouter, Depends
+from dependencies import get_current_user, require_role
+import schemas
+
+router = APIRouter()
+
+
+@router.get("/me", response_model=schemas.UserResponse)
+def get_me(current_user=Depends(get_current_user)):
+    return current_user
+
+
+@router.get("/admin")
+def admin_only(user=Depends(require_role("admin"))):
+    return {"message": "Welcome Admin!"}
